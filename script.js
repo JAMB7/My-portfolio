dfd9a71fc88e9031cc733e356f2aaa09fa084dcd
Julisha <busghitjam@gmail.com>
2026-05-21 18:04:45 -0300

    Test update

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 console.log("Portfolio website loaded!");
 
 window.addEventListener("load", () => {
-  document.querySelector("h1").innerText = "Hey Welcome, I'm Julisha✨";
+  const h1 = document.querySelector("h1");
+  const p = document.querySelector("p");
+
+  h1.innerText = "Hey Welcome to my portfolio ✨";
+  p.innerText = "I'm Julisha";
+
+  h1.style.opacity = 0;
+  p.style.opacity = 0;
+
+  setTimeout(() => {
+    h1.style.transition = "1.5s ease";
+    p.style.transition = "2s ease";
+
+    h1.style.opacity = 1;
+    p.style.opacity = 1;
+  }, 200);
 });
